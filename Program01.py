823e2c41c1d629835786ff553816df58f73a9dca
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-20 10:35:30 +0800

    B if elif statement

--- Program01.py
+++ Program01.py
@@ -5,18 +5,26 @@ d = int(input("Enter fourth number: "))
 
 if a > b and b > c and c > d:
     print(f"{a},{b},{c},{d}")
-
 elif a > b and b > d and d > c:
     print(f"{a},{b},{d},{c}")
-
 elif a > c and c > d and d > b:
     print(f"{a},{c},{d},{b}")
-
 elif a > c and c > b and b > d:
     print(f"{a},{c},{b},{d}")
-
 elif a > d and d > b and b > c:
     print(f"{a},{d},{b},{c}")
-
 elif a > d and d > c and c > b:
     print(f"{a},{d},{c},{b}")
+elif b > a and a > c and c > d:
+    print(f"{b},{a},{c},{d}")
+elif b > a and a > d and d > c:
+    print(f"{b},{a},{d},{c}")
+elif b > c and c > d and d > a:
+    print(f"{b},{c},{d},{a}")
+elif b > c and c > a and a > d:
+    print(f"{b},{c},{a},{d}")
+elif b > d and d > a and a > c:
+    print(f"{b},{d},{a},{c}")
+elif b > d and d > c and c > a:
+    print(f"{b},{d},{c},{a}")
+
