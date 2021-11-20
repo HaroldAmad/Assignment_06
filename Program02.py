e00883479a91923c9b0c84a266381ee0e3a8929a
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-20 11:57:33 +0800

    Number of Question for loop correction

--- Program02.py
+++ Program02.py
@@ -5,4 +5,13 @@ score = 0
 for i in range(10):
     first_number = random.randint(0,99)
     second_number = random.randint(0,99)
+    operator = ['+']
+    questions = str(first_number) +''+ str(operator) +''+ str(second_number)
+    answer = eval(questions)
+    questions+=': '
 
+    questions.update ({questions:str(answer)})
+
+for q in questions.keys():
+    user_answer = input (q)
+    if questions.get(q) == user_answer:
